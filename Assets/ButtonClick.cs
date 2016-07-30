8c590372446b8b63cfdee93e3d91997816162683
Kendall Weistroffer <kendall.fonebone@gmail.com>
2016-07-30 17:42:22 -0400

    added in title scene, instruction scene and button functionality

---
+++ Assets/ButtonClick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour {
+	public bool loadNextlvl = false;
+	public string levelName;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (loadNextlvl) {
+			//	Debug.Log ("changing to next lvl");
+			loadNextlvl = false;
+
+			SceneManager.LoadScene (levelName);
+		}
+
+	}
+
+	public void OnMyClick(){
+		loadNextlvl = true;
+		//Debug.Log ("Button Clicked");
+		//		Application.LoadLevel ("First Scene");
+	}
+}
