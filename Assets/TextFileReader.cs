34235eb0edc190b5cdf71e4a1da8ffe29ca2c41a
Kendall Weistroffer <kendall.fonebone@gmail.com>
2016-12-21 13:31:04 -0700

    re-duplicating second and third scenes

--- Assets/TextFileReader.cs
+++ Assets/TextFileReader.cs
@@ -4,9 +4,10 @@ using System.Collections;
 using System.IO;
 using System.Text;
 using UnityEngine.SceneManagement;
+using System;
 
-
-public class TextFileReader : MonoBehaviour {
+public class TextFileReader : MonoBehaviour
+{
 
 	public string filename;
 	public string nextLevel;
@@ -20,134 +21,171 @@ public class TextFileReader : MonoBehaviour {
 	public const string DATA_DIR = "Development";
 	private StringReader reader;
 	// Use this for initialization
-	void Start () {
-		shuffle(answerArray);
+	void Start ()
+	{
+
+		StartCoroutine (LoadText ());
+
+	}
+
+	IEnumerator LoadText ()
+	{
+		Debug.Log ("TFR.LoadText()");
+		yield return GetComponent<WWWFile> ().LoadText ();
+
+		Debug.Log ("Continuing TFR.LoadText()");
+
+		shuffle (answerArray);
 		//reader = GetStringReaderFromFilename (filename);
 		//readArray();
 
-
 		tArray = new Text[10];
-		tArray[0] = sentenceArray[0];
-		tArray[1] = answerArray[0];
-		tArray[2] = sentenceArray[1];
-		tArray[3] = answerArray[1];
-		tArray[4] = sentenceArray[2];
-		tArray[5] = answerArray[2];
-		tArray[6] = sentenceArray[3];
-		tArray[7] = answerArray[3];
-		tArray[8] = sentenceArray[4];
-		tArray[9] = answerArray[4];
+		tArray [0] = sentenceArray [0];
+		tArray [1] = answerArray [0];
+		tArray [2] = sentenceArray [1];
+		tArray [3] = answerArray [1];
+		tArray [4] = sentenceArray [2];
+		tArray [5] = answerArray [2];
+		tArray [6] = sentenceArray [3];
+		tArray [7] = answerArray [3];
+		tArray [8] = sentenceArray [4];
+		tArray [9] = answerArray [4];
 		reader = GetStringReaderFromFilename (filename);
-		readArray();
-
-
+		Debug.Log ("Received string reader from filename");
+		readArray ();
 	}
-	
+
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		//if (Input.GetButtonDown ("Jump") == true) { 
-			// read one line at a time if they press the space bar
+		// read one line at a time if they press the space bar
 		//string line = "";// = reader.ReadLine ();
-//		Debug.Log("Line: " + line);
-			//Text nextText = gameObject.AddComponent<Text> ();
-			//nextText.transform.parent = transform.FindChild ("Canvas");
+		//        Debug.Log("Line: " + line);
+		//Text nextText = gameObject.AddComponent<Text> ();
+		//nextText.transform.parent = transform.FindChild ("Canvas");
 		//nextText.text = line;
 
 
-	//		GameObject names = new GameObject();
-	//			names.text = line;
-			//textField.text = line;
-//			Debug.Log ("First line: " + line);
+		//        GameObject names = new GameObject();
+		//            names.text = line;
+		//textField.text = line;
+		//            Debug.Log ("First line: " + line);
 		//}
 
-		if (counter == tArray.Length/2) {
+		if (counter == tArray.Length / 2)
+		{
 			counter = 0;
-			Debug.Log("Loading: " + nextLevel);
-			SceneManager.LoadScene(nextLevel);
+			Debug.Log ("Loading: " + nextLevel);
+			SceneManager.LoadScene (nextLevel);
 		}
 	}
-/*	public string reverse(string s){
-		char[] ar = s.ToCharArray();
-		System.Array.Reverse(ar);
+	/*    public string reverse(string s){
+        char[] ar = s.ToCharArray();
+        System.Array.Reverse(ar);
 
 
-		return new string(ar);
+        return new string(ar);
 
-	}
+    }
 */
-	public int getCounter (){
+	public int getCounter ()
+	{
 		return counter;
 	}
 	//public  void setCounter(int counter){
-//		this.counter = counter;
+	//        this.counter = counter;
 	//}
-	public void shuffle(Text[] answers){
-		for (int i = 0; i < answers.Length; i++) {
-			Text temp = answers[i];
-			int r = Random.Range(i, answers.Length);
+	public void shuffle (Text[] answers)
+	{
+		for (int i = 0; i < answers.Length; i++)
+		{
+			Text temp = answers [i];
+			int r = UnityEngine.Random.Range (i, answers.Length);
 
-			answers[i] = answers[r];
-			answers[r] = temp;
+			answers [i] = answers [r];
+			answers [r] = temp;
 		}
-		
+
 	}
-	public void readArray(){
-		
+
+
+	public void readArray ()
+	{
+
 		string line = "";
 		string answer = "";
-	
-		for (int i = 0; i < tArray.Length; i+=2) {
 
-		
-			line = FlipFont.bob(reader.ReadLine()); 
-			tArray[i].text = line; 
+		for (int i = 0; i < tArray.Length; i += 2)
+		{
 
-			answer = FlipFont.bob(reader.ReadLine());
-				
-			tArray[i+1].text = answer;
 
-				tArray[i].GetComponentInParent<Trigger>().TheAnswer = answer;
+			line = FlipFont.bob (reader.ReadLine ()); 
+			//Debug.Log ("found line: " + line);
+			tArray [i].text = line; 
+
+
+			answer = FlipFont.bob (reader.ReadLine ());
+			tArray [i + 1].text = answer;
+
+
+			tArray [i].GetComponentInParent<Trigger> ().TheAnswer = answer;
 
 		}
 	}
 	// this just creates a string reader regardless of whether it's a file or not
-	public StringReader GetStringReaderFromFilename(string filename)
+	public StringReader GetStringReaderFromFilename (string filename)
 	{
 		//FileStream reader;
 		StreamReader reader;
 		/*FileInfo sourceFile = new FileInfo (Application.dataPath 
-			+ Path.DirectorySeparatorChar 
-			+ DATA_DIR 
-			+ Path.DirectorySeparatorChar
-			+ filename);
-			*/
+            + Path.DirectorySeparatorChar 
+            + DATA_DIR 
+            + Path.DirectorySeparatorChar
+            + filename);
+            */
 		string path = Application.dataPath
-		              + Path.DirectorySeparatorChar
-		              + DATA_DIR
-		              + Path.DirectorySeparatorChar
-		              + filename;
+			+ Path.DirectorySeparatorChar
+			+ DATA_DIR
+			+ Path.DirectorySeparatorChar
+			+ filename;
 
-		if (!Application.isWebPlayer) {
-			string filestr = File.ReadAllText(path, new UnicodeEncoding());
-			return new StringReader(filestr);
+		// note, just now always read from a URL
+		//if (!Application.isWebPlayer)
+		//{
+		//    string filestr = File.ReadAllText (path, new UnicodeEncoding ());
+		//    return new StringReader (filestr);
+		//
+		//}
+		//else
+		{
 
-		} else {
-			string filestr = GetComponent<WWWFile>().LoadText();
-			return new StringReader(filestr);
+			try
+			{
+				string filestr = GetComponent<WWWFile> ().GetText ();
+				return new StringReader (filestr);
+			}
+			catch (Exception e)
+			{
+				Debug.LogError ("Erorr creating the string reader: " + e);
+				return null;
+			}
+
 		}
 
 		//string filestr = File.ReadAllText(path, new UnicodeEncoding());
 		//if (sourceFile != null && sourceFile.Exists) {
-		//	UnicodeEncoding encoding = new UnicodeEncoding();
-		//	reader = sourceFile.OpenText();
+		//    UnicodeEncoding encoding = new UnicodeEncoding();
+		//    reader = sourceFile.OpenText();
 
-		//		return new StringReader(filestr);
-	/*
-		} else 
-		{
-			Debug.Log ("Unable to find " + Application.dataPath + "/" + filename);
-		}
-		return null;
-		*/
+		//        return new StringReader(filestr);
+		/*
+        } else 
+        {
+            Debug.Log ("Unable to find " + Application.dataPath + "/" + filename);
+        }
+        return null;
+        */
 	}
 }
--- Assets/WWWFile.cs
+++ Assets/WWWFile.cs
@@ -1,31 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Text;
+public class WWWFile : MonoBehaviour
+{
 
-public class WWWFile : MonoBehaviour {
-	public WWW file;
+	public string URL;
+	protected WWW file;
+	protected string unicodeString;
+
 
 	// Use this for initialization
-	void Start () {
-		LoadText();
+	void Start ()
+	{
+
 	}
-	public string LoadText(){
-		file = new WWW("http://www.cs.du.edu/~chrisg/hebrewgame/data/Hebrew3.txt");
 
-		while (true) {
+	public IEnumerator LoadText ()
+	{
+		// launch the coroutine to get the text
+		yield return StartCoroutine (GetData (URL));
+	}
 
-			if (file.isDone) {
-			
-				return file.text;
-			//	break;
-			}
+	public string GetText ()
+	{
+		return unicodeString; 
+	}
+
+	/** allow us to load the data from a URL in the background */
+	IEnumerator GetData (string url)
+	{
+
+		file = new WWW (url);
+		// this basically returns back to the main thread until it's finished loading
+		yield return file;
+
+		//Debug.Log ("Finished loading '" + url + "'");
+		//Debug.Log ("loaded " + file.bytes.Length + " bytes");
+
+		// take the text and parse it as unicode encoding
+
+		try
+		{
+			// interesting--needed to use BigEndianUnicode to make this work
+			unicodeString = Encoding.BigEndianUnicode.GetString (file.bytes);
+		}
+		catch (Exception e)
+		{
+			Debug.LogError ("Error trying to parse the unicode file: " + e);
 		}
 	}
 
-	
 	// Update is called once per frame
-	void Update () {
-		if (file.isDone) {
-			Debug.Log("done");
-		}
+	void Update ()
+	{
+
 	}
 }
